15ba4587838c5b1b1714c2b6b5322fe5006cb1be
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-11-01 00:37:35 +0100

    remove unused comments on top of the CommandArgument enum entries

--- src/main/kotlin/pl/jwizard/jwl/command/arg/CommandArgument.kt
+++ src/main/kotlin/pl/jwizard/jwl/command/arg/CommandArgument.kt
@@ -18,55 +18,15 @@ enum class CommandArgument(
 	val i18nSource: I18nCmdArgumentSource,
 	val arguments: Array<Argument>,
 ) {
-
-	/**
-	 * Argument for a track title or URL.
-	 */
 	TITLE_OR_URL(I18nCmdArgumentSource.TITLE_OR_URL, Argument.TRACK),
-
-	/**
-	 * Argument for the number of repeats.
-	 */
 	REPEATS_NUMBER(I18nCmdArgumentSource.REPEATS_NUMBER, Argument.COUNT),
-
-	/**
-	 * Argument for volume units.
-	 */
 	VOLUME_UNITS_NUMBER(I18nCmdArgumentSource.VOLUME_UNITS_NUMBER, Argument.VOLUME),
-
-	/**
-	 * Argument for tagging a member.
-	 */
 	MEMBER_TAG(I18nCmdArgumentSource.MEMBER_TAG, Argument.MEMBER),
-
-	/**
-	 * Argument for position in the queue.
-	 */
 	POSITION_IN_QUEUE(I18nCmdArgumentSource.POSITION_IN_QUEUE, Argument.POS),
-
-	/**
-	 * Arguments for the current and new position in the queue.
-	 */
 	CURRENT_AND_NEW_POS(I18nCmdArgumentSource.CURRENT_AND_NEW_POS, arrayOf(Argument.FROM_POS, Argument.TO_POS)),
-
-	/**
-	 * Argument for playlist name or ID.
-	 */
 	PLAYLIST_NAME_OR_ID(I18nCmdArgumentSource.PLAYLIST_NAME_OR_ID, Argument.PLAYLIST_NAME_OR_ID),
-
-	/**
-	 * Argument for the playlist name.
-	 */
 	PLAYLIST_NAME(I18nCmdArgumentSource.PLAYLIST_NAME, Argument.PLAYLIST_NAME),
-
-	/**
-	 * Argument for specifying a radio station.
-	 */
 	RADIO_STATION(I18nCmdArgumentSource.RADIO_STATION, Argument.RADIO_STATION),
-
-	/**
-	 * Argument for privacy settings.
-	 */
 	PRIVATE(I18nCmdArgumentSource.PRIVATE, Argument.PRIVATE);
 
 	/**
