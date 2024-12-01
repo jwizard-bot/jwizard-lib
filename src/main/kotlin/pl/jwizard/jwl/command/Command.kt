4aa9b6324c657d94277dc2b226967bb3a2ddb51c
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-12-01 04:42:39 +0100

    rewrite command keys to more readable format and remove shards command

--- src/main/kotlin/pl/jwizard/jwl/command/Command.kt
+++ src/main/kotlin/pl/jwizard/jwl/command/Command.kt
@@ -16,70 +16,69 @@ import pl.jwizard.jwl.i18n.I18nLocaleSource
  * @property dbId Unique identifier for the command in the database.
  * @property placeholder A string key used for localization, which maps to the actual command description.
  * @property module The module to which the command belongs.
- * @property alias The alias used to invoke the command.
  * @property argumentsDefinition Optional definition of command arguments.
+ * @property global Determines if command also can be executed in global environment (without guild reference).
  * @author Miłosz Gilga
  */
 enum class Command(
 	override val dbId: Long,
 	override val placeholder: String,
 	val module: Module,
-	val alias: String,
 	val argumentsDefinition: CommandArgument? = null,
+	val global: Boolean = false,
 ) : I18nLocaleSource, DatabaseIdentifier, TextKeyExtractor {
 
 	// music module
-	PLAY(1, "jw.command.play", Module.MUSIC, "pl", CommandArgument.TITLE_OR_URL),
-	PAUSE(2, "jw.command.pause", Module.MUSIC, "ps"),
-	RESUME(3, "jw.command.resume", Module.MUSIC, "rs"),
-	REPEAT(4, "jw.command.repeat", Module.MUSIC, "rp", CommandArgument.REPEATS_NUMBER),
-	REPEATCLS(5, "jw.command.repeatcls", Module.MUSIC, "rpcl"),
-	LOOP(6, "jw.command.loop", Module.MUSIC, "lp"),
-	PLAYING(7, "jw.command.playing", Module.MUSIC, "cp"),
-	PAUSED(8, "jw.command.paused", Module.MUSIC, "cps"),
-	GETVOLUME(9, "jw.command.getvolume", Module.MUSIC, "gvl"),
-	QUEUE(10, "jw.command.queue", Module.MUSIC, "qt"),
+	PLAY(1, "jw.command.play", Module.MUSIC, CommandArgument.TITLE_OR_URL),
+	PAUSE(2, "jw.command.pause", Module.MUSIC),
+	RESUME(3, "jw.command.resume", Module.MUSIC),
+	REPEAT_SET(4, "jw.command.repeat.set", Module.MUSIC, CommandArgument.REPEATS_NUMBER),
+	REPEAT_CLEAR(5, "jw.command.repeat.clear", Module.MUSIC),
+	INFINITE(6, "jw.command.infinite", Module.MUSIC),
+	PLAYING(7, "jw.command.playing", Module.MUSIC),
+	PAUSED(8, "jw.command.paused", Module.MUSIC),
+	VOLUME(9, "jw.command.volume", Module.MUSIC),
+	QUEUE_SHOW(10, "jw.command.queue.show", Module.MUSIC),
 
 	// dj module
-	SETVOLUME(101, "jw.command.setvolume", Module.DJ, "svl", CommandArgument.VOLUME_UNITS_NUMBER),
-	VOLUMECLS(102, "jw.command.volumecls", Module.DJ, "cvl"),
-	JOIN(103, "jw.command.join", Module.DJ, "jchn"),
-	TRACKSRM(104, "jw.command.tracksrm", Module.DJ, "rtr", CommandArgument.MEMBER_TAG),
-	SHUFFLE(105, "jw.command.shuffle", Module.DJ, "shq"),
-	SKIPTO(106, "jw.command.skipto", Module.DJ, "skt", CommandArgument.POSITION_IN_QUEUE),
-	SKIP(107, "jw.command.skip", Module.DJ, "sk"),
-	CLEAR(108, "jw.command.clear", Module.DJ, "cl"),
-	STOP(109, "jw.command.stop", Module.DJ, "st"),
-	MOVE(110, "jw.command.move", Module.DJ, "mv", CommandArgument.CURRENT_AND_NEW_POS),
-	INFINITE(111, "jw.command.infinite", Module.DJ, "inf"),
+	VOLUME_SET(101, "jw.command.volume.set", Module.DJ, CommandArgument.VOLUME_UNITS_NUMBER),
+	VOLUME_CLEAR(102, "jw.command.volume.clear", Module.DJ),
+	JOIN(103, "jw.command.join", Module.DJ),
+	QUEUE_REMOVE(104, "jw.command.queue.remove", Module.DJ, CommandArgument.MEMBER_TAG),
+	QUEUE_SHUFFLE(105, "jw.command.queue.shuffle", Module.DJ),
+	SKIPTO(106, "jw.command.skipto", Module.DJ, CommandArgument.POSITION_IN_QUEUE),
+	SKIP(107, "jw.command.skip", Module.DJ),
+	QUEUE_CLEAR(108, "jw.command.queue.clear", Module.DJ),
+	STOP(109, "jw.command.stop", Module.DJ),
+	QUEUE_MOVE(110, "jw.command.queue.move", Module.DJ, CommandArgument.CURRENT_AND_NEW_POS),
+	QUEUE_INFINITE(111, "jw.command.queue.infinite", Module.DJ),
 
-	// audio tracks
-	ADDTRACKPL(201, "jw.command.addtrackpl", Module.PLAYLIST, "apt", CommandArgument.PLAYLIST_NAME_OR_ID),
-	ADDQUEUEPL(202, "jw.command.addqueuepl", Module.PLAYLIST, "apq", CommandArgument.PLAYLIST_NAME_OR_ID),
-	ADDPLAYLIST(203, "jw.command.addplaylist", Module.PLAYLIST, "addpl", CommandArgument.PLAYLIST_NAME),
-	PLAYPL(204, "jw.command.playpl", Module.PLAYLIST, "pl", CommandArgument.PLAYLIST_NAME_OR_ID),
-	SHOWMEMPL(205, "jw.command.showmempl", Module.PLAYLIST, "smmpl", CommandArgument.MEMBER_TAG),
-	SHOWMYPL(206, "jw.command.showmypl", Module.PLAYLIST, "smpl"),
-	SHOWPLTRACKS(207, "jw.command.showplsongs", Module.PLAYLIST, "spltr", CommandArgument.PLAYLIST_NAME_OR_ID),
+	// playlists
+	PLAYLIST_ADDTRACK(201, "jw.command.playlist.add.track", Module.PLAYLIST, CommandArgument.PLAYLIST_NAME_OR_ID),
+	PLAYLIST_ADDQUEUE(202, "jw.command.playlist.add.queue", Module.PLAYLIST, CommandArgument.PLAYLIST_NAME_OR_ID),
+	PLAYLIST_CREATE(203, "jw.command.playlist.create", Module.PLAYLIST, CommandArgument.PLAYLIST_NAME),
+	PLAYLIST_PLAY(204, "jw.command.playlist.play", Module.PLAYLIST, CommandArgument.PLAYLIST_NAME_OR_ID),
+	PLAYLIST_MEMBER(205, "jw.command.playlist.member", Module.PLAYLIST, CommandArgument.MEMBER_TAG),
+	PLAYLIST_ME(206, "jw.command.playlist.me", Module.PLAYLIST),
+	PLAYLIST_TRACKS(207, "jw.command.playlist.tracks", Module.PLAYLIST, CommandArgument.PLAYLIST_NAME_OR_ID),
 
 	// vote
-	VSKIP(301, "jw.command.vskip", Module.VOTE, "vsk"),
-	VSKIPTO(302, "jw.command.vskipto", Module.VOTE, "vsto", CommandArgument.POSITION_IN_QUEUE),
-	VCLEAR(303, "jw.command.vclear", Module.VOTE, "vcl"),
-	VSTOP(304, "jw.command.vstop", Module.VOTE, "vst"),
-	VSHUFFLE(305, "jw.command.vshuffle", Module.VOTE, "vshq"),
+	VOTE_SKIP(301, "jw.command.vote.skip", Module.VOTE),
+	VOTE_SKIPTO(302, "jw.command.vote.skipto", Module.VOTE, CommandArgument.POSITION_IN_QUEUE),
+	VOTE_QUEUE_CLEAR(303, "jw.command.vote.queue.clear", Module.VOTE),
+	VOTE_STOP(304, "jw.command.vote.stop", Module.VOTE),
+	VOTE_QUEUE_SHUFFLE(305, "jw.command.vote.queue.shuffle", Module.VOTE),
 
 	// other
-	HELP(401, "jw.command.help", Module.OTHER, "h", CommandArgument.PRIVATE),
-	DEBUG(402, "jw.command.debug", Module.OTHER, "db", CommandArgument.PRIVATE),
-	SETTINGS(403, "jw.command.settings", Module.OTHER, "stg", CommandArgument.PRIVATE),
-	SHARDS(404, "jw.command.shards", Module.OTHER, "shr", CommandArgument.PRIVATE),
+	HELP(401, "jw.command.help", Module.OTHER, CommandArgument.PRIVATE, true),
+	DEBUG(402, "jw.command.debug", Module.OTHER, CommandArgument.PRIVATE),
+	SETTINGS(403, "jw.command.settings", Module.OTHER, CommandArgument.PRIVATE),
 
 	// radio
-	PLAYRADIO(501, "jw.command.radio", Module.RADIO, "rd", CommandArgument.RADIO_STATION),
-	STOPRADIO(502, "jw.command.radiostop", Module.RADIO, "rds"),
-	RADIOS(503, "jw.command.radios", Module.RADIO, "rda"),
-	RADIOINFO(504, "jw.command.radioinfo", Module.RADIO, "ri"),
+	RADIO_PLAY(501, "jw.command.radio.play", Module.RADIO, CommandArgument.RADIO_STATION),
+	RADIO_STOP(502, "jw.command.radio.stop", Module.RADIO),
+	RADIO_ALL(503, "jw.command.radio.all", Module.RADIO),
+	RADIO_INFO(504, "jw.command.radio.info", Module.RADIO),
 	;
 
 	/**
@@ -97,5 +96,5 @@ enum class Command(
 	fun parseWithPrefix(prefix: String) = "${prefix}$textKey"
 
 	override val textKey
-		get() = placeholder.substringAfterLast(".")
+		get() = placeholder.replace("jw.command.", "")
 }
