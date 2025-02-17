c9023be3161e0f702787b951324eb81d40df9b8a
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-17 23:37:14 +0100

    (fix) rename I18nLibDynamicSource to I18nFragmentSource and move project fragments into api project

--- src/main/kotlin/pl/jwizard/jwl/i18n/I18nFragmentSource.kt
+++ src/main/kotlin/pl/jwizard/jwl/i18n/I18nFragmentSource.kt
@@ -3,4 +3,4 @@ package pl.jwizard.jwl.i18n
 import pl.jwizard.jwl.property.Property
 
 // use this interface on dynamic i18n enum sources
-interface I18nDynamicSource : Property
+interface I18nFragmentSource : Property
