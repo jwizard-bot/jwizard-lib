5c601c79787787a0eef372af3b278b4339ccdecd
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-04-02 04:40:30 +0200

    (fix) add prefix to RQ queue names

--- src/main/kotlin/pl/jwizard/jwl/rabbitmq/RabbitQueue.kt
+++ src/main/kotlin/pl/jwizard/jwl/rabbitmq/RabbitQueue.kt
@@ -1,7 +1,7 @@
 package pl.jwizard.jwl.rabbitmq
 
 enum class RabbitQueue(val queueName: String) {
-	CORE_TO_API_LISTEN_STATS("core_to_api_listen_stats"),
-	CORE_TO_API_CMD_STATS("core_to_api_cmd_stats"),
+	JW_CORE_TO_API_LISTEN_STATS("jw_core_to_api_listen_stats"),
+	JW_CORE_TO_API_CMD_STATS("jw_core_to_api_cmd_stats"),
 	;
 }
