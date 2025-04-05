e30a8be3915077ebdcfb2c63d909bb2f0338ca20
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-04-05 22:07:59 +0200

    (refactor) create route data handler

---
+++ src/main/kotlin/pl/jwizard/jwl/server/route/handler/RouteDataHandler.kt
@@ -0,0 +1,17 @@
+package pl.jwizard.jwl.server.route.handler
+
+import io.javalin.http.Context
+import io.javalin.security.RouteRole
+import pl.jwizard.jwl.server.route.RouteMethod
+
+abstract class RouteDataHandler<T> : Handler() {
+	protected abstract val callback: (Context, T) -> Unit
+
+	protected abstract fun handleWithData(ctx: Context): T
+
+	override val withRoles = emptyMap<RouteRole, List<RouteMethod>>()
+
+	final override fun handle(ctx: Context) {
+		callback(ctx, handleWithData(ctx))
+	}
+}
